a2843b1038cdd57705523483660738e857c4f56f
samynko <samynko@domclick.ru>
2024-09-04 17:20:25 +0300

    added how to.txt

--- backend/app/ml_models/translation_model.py
+++ backend/app/ml_models/translation_model.py
@@ -1,24 +1,15 @@
-from transformers import MarianMTModel, MarianTokenizer
 from fastapi import HTTPException
 
 from app.schemas.translation import TranslationRequest
 
 
-def get_model_and_tokenizer(source_lang: str, target_lang: str):
-    model_name = f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}'
-    try:
-        tokenizer = MarianTokenizer.from_pretrained(model_name)
-        model = MarianMTModel.from_pretrained(model_name)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=f"Model loading failed: {str(e)}")
-    return model, tokenizer
-
+def get_model(source_lang: str, target_lang: str):
+    model = []
+    return model
 
 def perform_translation(request: TranslationRequest):
 
-    model, tokenizer = get_model_and_tokenizer(request.source_lang, request.target_lang)
-    inputs = tokenizer.encode(request.text, return_tensors="pt")
-    translated = model.generate(inputs)
-    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+    model = get_model(source_lang=request.source_lang, target_lang=request.target_lang)
+    traslated_text = f'Translation from {request.source_lang} to {request.target_lang}'
 
-    return translated_text
+    return traslated_text
